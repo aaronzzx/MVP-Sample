f57f1d97f933386eb8b311870d39bf3a719d1d95
Aaron <aaronzheng9603@gmail.com>
2019-01-02 23:17:48 +0800

    1.Lambda表达式

--- app/src/main/java/com/aaron/mvpsample/model/MainModel.java
+++ app/src/main/java/com/aaron/mvpsample/model/MainModel.java
@@ -9,35 +9,29 @@ public class MainModel implements IMainModel {
 
     @Override
     public void save(final Context context, final String data) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                SharedPreferences.Editor editor = context.getSharedPreferences
-                        ("data", Context.MODE_PRIVATE).edit();
-                editor.putString("input", data);
-                editor.apply();
-            }
+        new Thread(() -> {
+            SharedPreferences.Editor editor = context.getSharedPreferences
+                    ("data", Context.MODE_PRIVATE).edit();
+            editor.putString("input", data);
+            editor.apply();
         }).start();
     }
 
     @Override
     public void load(final Context context, final RequestListener listener) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                SharedPreferences preferences = context.getSharedPreferences
-                        ("data", Context.MODE_PRIVATE);
-                String get = preferences.getString("input", null);
-                if (!TextUtils.isEmpty(get)) {
-                    listener.onSuccess(get);
-                } else {
-                    listener.onFailed();
-                }
+        new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            SharedPreferences preferences = context.getSharedPreferences
+                    ("data", Context.MODE_PRIVATE);
+            String get = preferences.getString("input", null);
+            if (!TextUtils.isEmpty(get)) {
+                listener.onSuccess(get);
+            } else {
+                listener.onFailed();
             }
         }).start();
     }
